822d456cb97eb140a251f6a16dd0b33939efcda0
Yongjung Shin <yongshn220@gmail.com>
2024-01-24 01:33:48 -0500

    Add | dynamic metadata

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -1,8 +1,19 @@
 import EventPost from "@containers/events/id/EventPost";
-import Head from 'next/head';
-import React, {Fragment} from "react";
 import {IEventPost} from "@models/collections/eventPost";
 import getEventPostAction from "@actions/event/getEventPostAction";
+import {Metadata, ResolvingMetadata} from "@node_modules/next";
+
+
+export async function generateMetadata({ params }, parent: ResolvingMetadata): Promise<Metadata> {
+  const postId = params.id
+  const post: IEventPost  = await getEventPostAction(postId)
+  if (!post) return {}
+
+  return {
+    title: post.title,
+    description: post.description,
+  }
+}
 
 
 export default async function EventPostPage({ params }) {
@@ -12,12 +23,6 @@ export default async function EventPostPage({ params }) {
   if (!post) return <></>
 
   return (
-    <Fragment>
-      <Head>
-        <title>{post.title}</title>
-        <meta name="description" content={post.description}/>
-      </Head>
-      <EventPost post={post}/>
-    </Fragment>
+    <EventPost post={post}/>
   )
 }
