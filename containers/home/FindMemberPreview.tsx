367c71f4539374254a94736283fb3a86df17e21b
Yongjung Shin <yongshn220@gmail.com>
2024-01-25 21:22:17 -0500

    dep tst

--- containers/home/FindMemberPreview.tsx
+++ containers/home/FindMemberPreview.tsx
@@ -10,30 +10,30 @@ import {ICommunityPost} from "@models/union/union";
 const DEFAULT_COMMUNITY_TYPE = CommunityType.EVERYONE
 
 export default async function FindMemberPreview() {
-  // const postIds = await getCommunityPostIdsAction(PostType.FIND_MEMBER, DEFAULT_COMMUNITY_TYPE, 1)
-  //
-  // const posts = []
-  // for (const id of postIds) {
-  //   const post = await getCommunityPostAction(id, PostType.FIND_MEMBER) as ICommunityPost
-  //   if (!post) continue
-  //
-  //   posts.push(post)
-  // }
+  const postIds = await getCommunityPostIdsAction(PostType.FIND_MEMBER, DEFAULT_COMMUNITY_TYPE, 1)
+
+  const posts = []
+  for (const id of postIds) {
+    const post = await getCommunityPostAction(id, PostType.FIND_MEMBER) as ICommunityPost
+    if (!post) continue
+
+    posts.push(post)
+  }
 
 
   return (
     <div className="relative">
-      {/*<Link href={getCommunityHomePath(PostType.FIND_MEMBER, DEFAULT_COMMUNITY_TYPE)}>*/}
-      {/*  <div className="glass_box cursor-pointer group">*/}
-      {/*    <p className={`text-md font-semibold hover_text_blue`}>Find Member</p>*/}
-      {/*  </div>*/}
-      {/*</Link>*/}
+      <Link href={getCommunityHomePath(PostType.FIND_MEMBER, DEFAULT_COMMUNITY_TYPE)}>
+        <div className="glass_box cursor-pointer group">
+          <p className={`text-md font-semibold hover_text_blue`}>Find Member</p>
+        </div>
+      </Link>
 
       <ul role="list" className="divide-y divide-gray-300 border-b">
         {
-          // posts.slice(0, 5).map((post) => (
-          //   <CommunityPostItem key={post.id} postType={PostType.FIND_MEMBER} communityType={DEFAULT_COMMUNITY_TYPE} post={post}/>
-          // ))
+          posts.slice(0, 5).map((post) => (
+            <CommunityPostItem key={post.id} postType={PostType.FIND_MEMBER} communityType={DEFAULT_COMMUNITY_TYPE} post={post}/>
+          ))
         }
       </ul>
     </div>
--- containers/home/GeneralPreview.tsx
+++ containers/home/GeneralPreview.tsx
@@ -9,30 +9,30 @@ import {ICommunityPost} from "@models/union/union";
 const DEFAULT_COMMUNITY_TYPE = CommunityType.EVERYONE
 
 export default async function GeneralPreview() {
-  // const postIds = await getCommunityPostIdsAction(PostType.GENERAL, DEFAULT_COMMUNITY_TYPE, 1)
-  //
-  // const posts = []
-  // for (const id of postIds) {
-  //   const post = await getCommunityPostAction(id, PostType.GENERAL) as ICommunityPost
-  //   if (!post) continue
-  //
-  //   posts.push(post)
-  // }
+  const postIds = await getCommunityPostIdsAction(PostType.GENERAL, DEFAULT_COMMUNITY_TYPE, 1)
+
+  const posts = []
+  for (const id of postIds) {
+    const post = await getCommunityPostAction(id, PostType.GENERAL) as ICommunityPost
+    if (!post) continue
+
+    posts.push(post)
+  }
 
 
   return (
     <div className="relative">
-      {/*<Link href={getCommunityHomePath(PostType.GENERAL, DEFAULT_COMMUNITY_TYPE)}>*/}
-      {/*  <div className="glass_box cursor-pointer group">*/}
-      {/*    <p className={`text-md font-semibold hover_text_blue`}>General</p>*/}
-      {/*  </div>*/}
-      {/*</Link>*/}
+      <Link href={getCommunityHomePath(PostType.GENERAL, DEFAULT_COMMUNITY_TYPE)}>
+        <div className="glass_box cursor-pointer group">
+          <p className={`text-md font-semibold hover_text_blue`}>General</p>
+        </div>
+      </Link>
 
       <ul role="list" className="divide-y divide-gray-300 border-b">
         {
-          // posts.slice(0, 5).map((post) => (
-          //   <CommunityPostItem key={post.id} postType={PostType.GENERAL} communityType={DEFAULT_COMMUNITY_TYPE} post={post}/>
-          // ))
+          posts.slice(0, 5).map((post) => (
+            <CommunityPostItem key={post.id} postType={PostType.GENERAL} communityType={DEFAULT_COMMUNITY_TYPE} post={post}/>
+          ))
         }
       </ul>
     </div>
